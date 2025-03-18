92002e2ed88e3f288601f54dd9abaf782da084db
Phuc Pham <29089718+paphuc@users.noreply.github.com>
2025-03-18 23:32:39 +0700

    init project

---
+++ src/main/java/com/example/pap_shop/PapShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.pap_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PapShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pap_shop/PapShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pap_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
